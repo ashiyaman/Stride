58ef39854a1bea2d10502c24606948f014272808
ashiyaman <ashiya1005@gmail.com>
2025-02-21 10:19:23 +0530

    chore: Add html structure

---
+++ actions.js
@@ -0,0 +1,5 @@
+export const ADD_TASK = "task/added" 
+export const REMOVE_TASK = "task/removed" 
+export const TOGGLE_TASK = "task/toggle" 
+export const CALCULATE_TOTAL_TASKS = "totalTask/calculate"
+
