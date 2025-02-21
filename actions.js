10568626621e36c07f8908e14558840cc308abae
ashiyaman <ashiya1005@gmail.com>
2025-02-21 16:11:36 +0530

    chore: Add action creators

--- actions.js
+++ actions.js
@@ -3,3 +3,21 @@ export const REMOVE_TASK = "task/removed"
 export const TOGGLE_TASK = "task/toggle" 
 export const CALCULATE_TOTAL_TASKS = "totalTask/calculate"
 
+export const addTask = (task) => ({
+    type: "ADD_TASK",
+    payload: task
+})
+
+export const removeTask = (taskId) => ({
+    type: "REMOVE_TASK",
+    payload: taskId
+})
+
+export const toggleTask = (task) => ({
+    type: "TOGGLE_TASK",
+    payload: task
+})
+
+export const calculateTotalTasks  = () => ({
+    type: "CALCULATE_TOTAL_TASKS"
+})
